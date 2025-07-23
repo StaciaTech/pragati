41e464e1578972ed9d092c01eb0ce6b2467b47e8
Revanth <revanth6698@gmail.com>
2025-07-23 07:38:03 +0000

    i said decrease but u r increasing the whitespace

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -289,7 +289,7 @@ const SidebarInset = React.forwardRef<
       ref={ref}
       className={cn(
         "relative flex min-h-svh flex-1 flex-col bg-background/90",
-        "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:ml-0 md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
+        "peer-data-[variant=inset]:min-h-[calc(100svh-theme(spacing.4))] md:peer-data-[variant=inset]:rounded-xl md:peer-data-[variant=inset]:shadow",
         "transition-all duration-300 ease-in-out",
         "peer-data-[state=expanded]:md:ml-[--sidebar-width]",
         "peer-data-[state=collapsed]:md:ml-[--sidebar-width-icon]",
