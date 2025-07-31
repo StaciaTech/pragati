885126cd3eb7e6b14a79741633d4ffe83d7fd5a1
Sarabesh Sriram <sarabeshsriram@gmail.com>
2025-07-31 05:40:26 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Primitive.button

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -3,7 +3,7 @@
 
 import * as React from "react"
 import * as AccordionPrimitive from "@radix-ui/react-accordion"
-import { ChevronRight } from "lucide-react"
+import { ChevronDown } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
@@ -29,13 +29,13 @@ const AccordionTrigger = React.forwardRef<
     <AccordionPrimitive.Trigger
       ref={ref}
       className={cn(
-        "flex flex-1 items-center gap-2 py-4 font-medium transition-all hover:no-underline [&[data-state=open]>svg:last-child]:rotate-90",
+        "flex flex-1 items-center gap-2 py-4 font-medium transition-all hover:no-underline [&[data-state=open]>svg:last-child]:-rotate-180",
         className
       )}
       {...props}
     >
       {children}
-      <ChevronRight className="h-4 w-4 shrink-0 transition-transform duration-200" />
+      <ChevronDown className="h-4 w-4 shrink-0 transition-transform duration-200" />
     </AccordionPrimitive.Trigger>
   </AccordionPrimitive.Header>
 ))
